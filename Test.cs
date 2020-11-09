446b46ad47fb0b6e5ad7885404f8f61ae1668794
Lasse Foster <lasse.foster@web.de>
2020-11-09 17:41:02 +0100

    + init upload and test scripts

---
+++ Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using USCSL;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(USCSL_Library.Test());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ USCSL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace USCSL
+{
+    public static class USCSL_Library
+    {
+        public static bool Test()
+        {
+            return true;
+        }
+    }
+}
