ea4b2f8e6ffa6984323ab7456a0dcd498cf05243
Lasse Foster <lasse.foster@web.de>
2022-01-19 19:43:36 +0100

    Fixed numerous bugs in the native array extensions and added support for readwrite and readonly references.

--- Extensions/Extensions_NativeArray.cs
+++ Extensions/Extensions_NativeArray.cs
@@ -12,39 +12,73 @@ namespace USCSL
         {
             public IntPtr ptr;
             public int length;
+            public AtomicSafetyHandle safetyHandle;
 
-            public NativeArrayRef(IntPtr ptr, int length)
+            public NativeArrayRef(IntPtr ptr, int length, AtomicSafetyHandle safetyHandle)
             {
                 this.ptr = ptr;
                 this.length = length;
+                this.safetyHandle = safetyHandle;
             }
+        }
+
+        [BurstDiscard]
+        public static unsafe void GetSafetyHandle<T>(ref NativeArray<T> array, out AtomicSafetyHandle handle)
+            where T : struct
+        {
+            handle = NativeArrayUnsafeUtility.GetAtomicSafetyHandle(array);
         }
         
         [BurstDiscard]
-        public static void GetRef<T>(ref NativeArray<T> array, out NativeArrayRef @ref) where T : struct
+        public static void GetReadWriteRef<T>(ref NativeArray<T> array, out NativeArrayRef @ref) where T : struct
         {
-            GetIntPtr(ref array, out var ptr);
-            @ref = new NativeArrayRef(ptr, array.Length);
+            GetReadWriteIntPtr(ref array, out var ptr);
+            GetSafetyHandle(ref array, out var handle);
+            @ref = new NativeArrayRef(ptr, array.Length, handle);
+        }
+        
+        [BurstDiscard]
+        public static void GetReadonlyRef<T>(ref NativeArray<T> array, out NativeArrayRef @ref) where T : struct
+        {
+            GetReadonlyIntPtr(ref array, out var ptr);
+            GetSafetyHandle(ref array, out var handle);
+            @ref = new NativeArrayRef(ptr, array.Length, handle);
         }
 
         [BurstDiscard]
-        public static unsafe void ToArray<T>(ref NativeArrayRef arrayRef, Allocator allocator, out NativeArray<T> array)
+        public static unsafe void ToNativeArray<T>(ref NativeArrayRef arrayRef, out NativeArray<T> array)
             where T : struct
         {
-            array = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<T>(arrayRef.ptr.ToPointer(),
-                arrayRef.length, allocator);
+            NativeArray<T> nativeArray = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<T>(arrayRef.ptr.ToPointer(),
+                arrayRef.length, Allocator.Invalid);
+            NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref nativeArray, arrayRef.safetyHandle);
+            array = nativeArray;
         }
 
         [BurstDiscard]
-        public static unsafe void GetIntPtr<T>(ref NativeArray<T> array, out IntPtr ptr) where T : struct
+        public static unsafe void ToNativeReadonlyArray<T>(ref NativeArrayRef arrayRef,
+            out NativeArray<T>.ReadOnly array) where T : struct
+        {
+            ToNativeArray<T>(ref arrayRef, out var readWriteArray);
+            array = readWriteArray.AsReadOnly();
+        }
+
+        [BurstDiscard]
+        public static unsafe void GetReadWriteIntPtr<T>(ref NativeArray<T> array, out IntPtr ptr) where T : struct
         {
             ptr = new IntPtr(array.GetUnsafePtr());
+        }
+
+        [BurstDiscard]
+        public static unsafe void GetReadonlyIntPtr<T>(ref NativeArray<T> array, out IntPtr ptr) where T : struct
+        {
+            ptr = new IntPtr(array.GetUnsafeReadOnlyPtr());
         }
 
         [BurstCompile]
         public static int SumInts(ref NativeArrayRef arrayRef)
         {
-            ToArray<int>(ref arrayRef, Allocator.Temp, out var array);
+            ToNativeArray<int>(ref arrayRef, out var array);
             int sum = 0;
             for (int i = 0; i < array.Length; i++)
             {
@@ -56,13 +90,14 @@ namespace USCSL
         
         [BurstCompile]
         public static double SumDoubles(ref NativeArrayRef arrayRef)
-        { 
-            ToArray<double>(ref arrayRef, Allocator.Temp, out var array);
+        {
+            ToNativeArray<double>(ref arrayRef, out var array);
             double sum = 0;
             for (int i = 0; i < array.Length; i++)
             {
                 sum += array[i];
             }
+            
             return sum;
         }
     }
---
+++ Extensions/Extensions_NativeHashSet.cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace USCSL
+{
+    public partial class Extensions
+    {
+        
+    }
+}
