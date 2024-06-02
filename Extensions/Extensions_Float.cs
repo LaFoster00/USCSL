d453518b0ee661f85637c3f6ad64bbb1e5d75977
Lasse Foster <lasse.foster@web.de>
2024-06-02 21:17:03 +0200

    * added next after for singles and doubles

--- Extensions/Extensions_Float.cs
+++ Extensions/Extensions_Float.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,44 @@ namespace USCSL
         public static float Map(this float s, float min, float max, float newMin, float newMax)
         {
             return newMin + (s - min) * (newMax - newMin) / (max - min);
+        }
+
+        public static float NextAfter(this float from, float to)
+        {
+            if (float.IsNaN(from) || float.IsNaN(to)) return float.NaN;
+
+            if (from == to) return to;
+
+            int bits = BitConverter.SingleToInt32Bits(from);
+            int direction = (from < to) ? 1 : -1;
+
+            // If from is zero, handle it as a special case
+            if (from == 0.0)
+            {
+                return BitConverter.Int32BitsToSingle(direction);
+            }
+
+            int nextBits = bits + direction;
+            return BitConverter.Int32BitsToSingle(nextBits);
+        }
+        
+        public static double NextAfter(this double from, double to)
+        {
+            if (double.IsNaN(from) || double.IsNaN(to)) return double.NaN;
+
+            if (from == to) return to;
+
+            long bits = BitConverter.DoubleToInt64Bits(from);
+            long direction = (from < to) ? 1 : -1;
+
+            // If from is zero, handle it as a special case
+            if (from == 0.0)
+            {
+                return BitConverter.Int64BitsToDouble(direction);
+            }
+
+            long nextBits = bits + direction;
+            return BitConverter.Int64BitsToDouble(nextBits);
         }
     }
 }
